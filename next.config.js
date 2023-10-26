098a3a4b525801188e1aa37c42c9fc845b11171c
Johannes Naylor <jonaylor89@gmail.com>
2023-10-26 12:10:46 -0500

    add image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
+        domains: [
+            'localhost',
+            'epk.tapped.ai',
+        ],
     }
 }
 
