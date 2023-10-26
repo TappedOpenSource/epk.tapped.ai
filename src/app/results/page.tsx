5b4df4c5ba53210a3e3fcbcca05fd62097241fc5
Johannes Naylor <jonaylor89@gmail.com>
2023-10-26 12:05:30 -0500

    url help

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,16 +1,19 @@
+import { getURL } from '@/utils/url';
 import Image from 'next/image';
-
+import { useRouter } from 'next/navigation';
 
 export default function Results() {
+    const imageUrl = getURL('/epk');
     return (
         <div
             className="min-h-screen flex flex-col justify-center items-center"
         >
             <Image 
-                src={`/epk`}
+                src={imageUrl}
                 alt="EPK"
                 width={256}
                 height={256}
+                priority
             />
         </div>
     );
