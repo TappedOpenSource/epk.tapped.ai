3e3a193bd6bac3aa6dac16e3b5588a82eb120c86
Johannes Naylor <jonaylor89@gmail.com>
2023-10-27 15:59:17 -0500

    loading page

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,11 +1,29 @@
+'use client';
+
+import { UserModel } from '@/types/user_model';
 import { getURL } from '@/utils/url';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Results() {
 
-    
+    const [user, setUser] = useState<UserModel | null>(null);
+    useEffect(() => {
+        console.log('get logged in user data')
+    }, []);
 
     const imageUrl = getURL('/epk');
+
+    if (user === null) {
+        return (
+            <>
+                <div className='min-h-screen flex justify-center items-center'>
+                    <p>loading...</p>
+                </div>
+            </>
+        );
+    }
+
     return (
         <div
             className="min-h-screen flex flex-col justify-center items-center"
