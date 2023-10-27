ae6ab3087b719fbab5c73e60e93c4b2275ef19dd
Johannes Naylor <jonaylor89@gmail.com>
2023-10-26 21:24:50 -0500

    landing page init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,34 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
+    <>
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {/* Nav Bar */}
       <h1
         className='text-4xl font-extrabold text-center'
       >
-        EPK Generator
+        present yourself like a professional
       </h1>
-      <div className="h-8" />
-      <Link
-        href="/results"
-        className='bg-black text-white font-extrabold rounded-full px-4 py-2'
-      >
-        results
+      <h2>
+        build an electronic press kit and get booked for live performances
+      </h2>
+      <div className='h-8' />
+      <Link 
+        href="https://tapped.ai"
+        className='bg-blue-500 text-white font-extrabold px-4 py-2 rounded-full'>
+        get started
       </Link>
     </main>
+    <section>
+      <div className='flex flex-col md:flex-row justify-around p-24'>
+        <div>
+          testimonial from Niral
+        </div>
+        <div>
+          image of Nirals EPK
+        </div>
+      </div>
+    </section>
+    </>
   );
 }
