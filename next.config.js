efbf242f4e181fc0b3866752c0ec676725d4ec5b
Johannes Naylor <jonaylor89@gmail.com>
2023-10-28 13:32:30 -0500

    add image url

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'epk.tapped.ai',
             },
+            {
+                protocol: 'https',
+                hostname: 'getmusicepk.com',
+            },
         ],
     }
 }
