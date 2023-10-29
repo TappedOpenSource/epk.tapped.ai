11e3837f16719556a72f309ac22c604964c5458e
Johannes Naylor <jonaylor89@gmail.com>
2023-10-29 16:44:29 -0500

    add www

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'getmusicepk.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'www.getmusicepk.com',
+            },
         ],
     }
 }
