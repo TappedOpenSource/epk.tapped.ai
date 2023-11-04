1011abb6e015873ac053c0af8de640ecac5cee77
Johannes Naylor <jonaylor89@gmail.com>
2023-11-03 19:13:52 -0500

    add field if not there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'  
+import Link from 'next/link'
 import Image from 'next/image';
 import Nav from '@/components/Nav';
 
@@ -6,47 +6,49 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <main className="flex flex-col md:items-start justify-center px-4 md:p-24">
-        {/* Nav Bar */}
-        <h1
-          className='uppercase text-center md:text-start text-4xl md:text-9xl font-black w-3/4'
-        >
-          your {' '}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-sky-300">
-            electronic press kit
-          </span>
-        </h1>
-        <h2 className='text-lg md:text-xl text-extrathin text-center md:text-start'>
-          create an electronic press kit and get booked for live performances just like the pros
-        </h2>
-        <div className='h-8' />
-        <Link
-          href="/login?returnUrl=/epk_form"
-          className='bg-blue-700 text-white font-extrabold px-4 py-2 rounded-full'>
+      <div className='px-4 md:p-24'>
+        <main className="flex flex-col items-center md:items-start justify-center">
+          {/* Nav Bar */}
+          <h1
+            className='uppercase text-center md:text-start text-4xl md:text-9xl font-black w-3/4'
+          >
+            your {' '}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-sky-300">
+              electronic press kit
+            </span>
+          </h1>
+          <h2 className='text-lg md:text-xl text-extrathin text-center md:text-start'>
+            create an electronic press kit and get booked for live performances just like the pros
+          </h2>
+          <div className='h-8' />
+          <Link
+            href="/login?returnUrl=/epk_form"
+            className='bg-blue-700 text-white font-extrabold px-4 py-2 rounded-full'>
             get started
-        </Link>
-      </main>
-      <section>
-        <div className='relative flex flex-col justify-center items-center p-24'>
-          <Image
-            src="/images/diagram.png"
-            alt="diagram of epk generator"
-            fill={true}
-            // layout='responsive'
-            objectFit='contain'
-          />
-        </div>
-      </section>
-      <section>
-        <div className='flex flex-col md:flex-row justify-around p-24'>
-          <div>
-            testimonial from Niral
+          </Link>
+        </main>
+        <section>
+          <div className='relative flex flex-col justify-center items-center p-24'>
+            <Image
+              src="/images/diagram.png"
+              alt="diagram of epk generator"
+              fill={true}
+              // layout='responsive'
+              objectFit='contain'
+            />
           </div>
-          <div>
-            image of Nirals EPK
+        </section>
+        <section>
+          <div className='flex flex-col md:flex-row justify-around p-24'>
+            <div>
+              testimonial from Niral
+            </div>
+            <div>
+              image of Nirals EPK
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 }
