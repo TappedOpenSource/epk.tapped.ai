3f7ff5b7ad22fb24231740eedee0a10abdab4a7e
Johannes Naylor <jonaylor89@gmail.com>
2023-11-06 20:38:44 -0600

    logout on loading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,9 +1,17 @@
+import { logout } from "@/utils/auth";
 
 export default function Loading() {
     return (
         <>
             <div className="min-h-screen flex justify-center items-center">
                 <p>loading...</p>
+                <div className="h-4" />
+                <button
+                    onClick={() => logout()}
+                    className='text-gray-500 px-4 py-2'
+                >
+                    logout
+                </button>
             </div>
         </>
     );
