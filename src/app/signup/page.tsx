50a86b1cdca4b98c826b6c9da56118833500c29f
Johannes Naylor <jonaylor89@gmail.com>
2023-11-08 21:48:58 -0600

    logout help

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,15 @@
 
 import { logout } from "@/utils/auth";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function SignUp() {
+    const router = useRouter();
+    const onLogout = async () => {
+        await logout();
+        router.push("/");
+    }
+
     return (
         <>
             <div className="min-h-screen flex flex-col justify-center items-center">
@@ -22,7 +29,7 @@ export default function SignUp() {
                 </Link>
                 <div className="h-4" />
                 <button
-                    onClick={() => logout()}
+                    onClick={onLogout}
                     className='text-gray-500 px-4 py-2'
                 >
                     logout
--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,13 +1,20 @@
 import { logout } from "@/utils/auth";
+import { useRouter } from "next/router";
 
 export default function Loading() {
+    const router = useRouter();
+    const onLogout = async () => {
+        await logout();
+        router.push("/");
+    }
+
     return (
         <>
             <div className="min-h-screen flex flex-col justify-center items-center">
                 <p>loading...</p>
                 <div className="h-4" />
                 <button
-                    onClick={() => logout()}
+                    onClick={onLogout}
                     className='text-gray-500 px-4 py-2'
                 >
                     logout
